cffe28df4f3e3daae3a50a5ee044ddc8c4d69726
Scoty Stobbe <sstobbe@1800contacts.com>
2025-05-17 23:07:40 -0600

    forgot something

--- api/test/[...catchall].js
+++ api/test/[...catchall].js
@@ -1,13 +1,13 @@
 module.exports = (req, res) => {
   const url = req.url;
-  const method = req.method;
+  const matched = url.replace(/^\/api\/test\/?/, '').split('/').filter(Boolean);
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-
   res.end(JSON.stringify({
     message: 'Catch-all route hit!',
+    method: req.method,
     url,
-    method
+    matchedSegments: matched
   }));
 };
