e2eada79d9ce0b17d337ec5b27be2a4fefc411f4
Scoty Stobbe <scotystobbe@gmail.com>
2026-01-09 10:08:41 -0700

    fix safe area and previous track

--- src/data/usePrevTrackStore.js
+++ src/data/usePrevTrackStore.js
@@ -1,10 +1,18 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const usePrevTrackStore = create(set => ({
-  prevTrack: null,
-  prevDbSong: null,
-  setPrevTrack: (track) => set({ prevTrack: track }),
-  setPrevDbSong: (dbSong) => set({ prevDbSong: dbSong }),
-}));
+const usePrevTrackStore = create(
+  persist(
+    (set) => ({
+      prevTrack: null,
+      prevDbSong: null,
+      setPrevTrack: (track) => set({ prevTrack: track }),
+      setPrevDbSong: (dbSong) => set({ prevDbSong: dbSong }),
+    }),
+    {
+      name: 'prev-track-storage', // unique name for localStorage key
+    }
+  )
+);
 
 export default usePrevTrackStore; 
